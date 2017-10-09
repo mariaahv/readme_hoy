1fdb771396b4205668b25dbbf1a775c4bb98f64e
mariaahv <mariahv14@hotmail.com>
2017-10-09 12:38:00 -0500

    cifrado cesar

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-# esto es un titulo #
-###### maria######
-* lalalala
-*mamamama
+#  CIFRADO CESAR #
+## PSEUDOODIGO ##
+### A continuacion les presento mi PSEUDOODIGO:
+###  => algoritmo CESAR
+## variables :
+### * letra
+### * positionLetra
+### * valorfijo
+### * Positionasci
+### * nuevaletra
+# INICIO
+##  Escribir(" ingrese la palabra o frase ")
+##  Leer letra
+##  Buscamos el indice o posicion de la letra (positionLetra)
+##  aplicamos la formula de cesar para obtenerlo en el asci
+## * formula= (positionLetra + Valorfijo)%26
+## imprime la posicion en el codigo asci de la letra ingresada
+## aplicamos la formula para hallar la nuevaletra ,convertindo de numero a letra
+## nuevaletra=(Positionasci  - 65 + Valorfijo)%26 + 65)
+##  Imprime output .
+## escribir( "la nueva letra es: output")
+# FIN
--- js/app.js
+++ js/app.js
@@ -1,28 +1,30 @@
-function getCash(atm, money) {
-  var cash = [],
-    centinel = false,
-    currentMoney = money;
+//function cipher(atm, money) {
+//crear una variable y asignar el abecedario
+  var alphabet='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//crear arr ;donde almacenare el str convertido a un arreglo
+  var arr=alphabet.split('');
+  var positionAsci=0;;
+  var newPosition=0;
+  var positionIndex=0;
+  var newPosition="";
+  var newLetter='';
 
-  for (var i = 0; i < atm.length && !centinel; i++) {
-    var div = 0;
-    var papers = 0;
-
-    if (money > 0) {
-      div = parseInt(money / atm[i].amount);
-
-      if (div > atm[i].quantity) {
-        papers = atm[i].quantity;
-        atm[i].quantity -= papers;
-      } else {
-        papers = div;
-        atm[i].quantity -= div;
-      }
-
-      cash.push({ amount: atm[i].amount, quantity: papers });
-      money -= (atm[i].amount * papers);
-    } else {
-      centinel = true;
-    }
-  }
-  return cash;
-};
+  // El usuario ingresara la palabra
+  var input = prompt("Ingrese la palabra:");
+  //con un for recorreremos la palabra dada
+  for (var i = 0; i < input.length ; i++) {
+  //charAt me devuelve la letra con el indice indicado Ejm A
+  //charCodeAt devuelve elvalor correspondiente de la letra segun ascii EJM 65
+  positionAsci=(input.charAt(i)).charCodeAt();
+  //positionAsci almacena el numero ascii de cada letra
+  positionIndex=arr.indexOf(input.charAt(i));
+  // charAt devuelve la letra con el indice dado
+  //indexOf devuelve el indice de dicha letra
+  //asi obtenemos con positionIndex la posicion(indice) de cada letra
+  newPosition=(positionIndex + 33) % 26;
+  //aplicamos la formula de cesar
+  newLetter+=arr[newPosition];
+  //en newLetter obtenemos la
+  console.log(newLetter)
+}
+//};
