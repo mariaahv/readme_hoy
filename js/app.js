fa780fd04a37c1ce8d993dfaf7225e36de3b9ef6
mariaahv <mariahv14@hotmail.com>
2017-10-10 12:24:01 -0500

    cifrado cesar

--- js/app.js
+++ js/app.js
@@ -1,30 +1,40 @@
-//function cipher(atm, money) {
-//crear una variable y asignar el abecedario
-  var alphabet='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-//crear arr ;donde almacenare el str convertido a un arreglo
-  var arr=alphabet.split('');
-  var positionAsci=0;;
-  var newPosition=0;
-  var positionIndex=0;
-  var newPosition="";
-  var newLetter='';
 
-  // El usuario ingresara la palabra
-  var input = prompt("Ingrese la palabra:");
-  //con un for recorreremos la palabra dada
-  for (var i = 0; i < input.length ; i++) {
+function cipher(word){
+ var str='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//creamos un string y le asignamos el abecedario
+
+var array=str.split('');
+// convertimos el string a un array
+var newLetter='';
+
+do{
+  //ingresamos la palabra
+  var word=prompt('Ingrese texto');
+
+  // validamos que no se ingresen numeros ,ni sea vacio
+}while(!isNaN(word) || word.length===0);
+for(var i=0;i<word.length;i++)
+//recorremos el  indice de la palabra dada
+{
+
   //charAt me devuelve la letra con el indice indicado Ejm A
-  //charCodeAt devuelve elvalor correspondiente de la letra segun ascii EJM 65
-  positionAsci=(input.charAt(i)).charCodeAt();
-  //positionAsci almacena el numero ascii de cada letra
-  positionIndex=arr.indexOf(input.charAt(i));
-  // charAt devuelve la letra con el indice dado
-  //indexOf devuelve el indice de dicha letra
-  //asi obtenemos con positionIndex la posicion(indice) de cada letra
-  newPosition=(positionIndex + 33) % 26;
-  //aplicamos la formula de cesar
-  newLetter+=arr[newPosition];
-  //en newLetter obtenemos la
-  console.log(newLetter)
+  //positionAsci devuelve elvalor correspondiente segun ascii EJM 65
+  positionAsci=(word.charAt(i)).charCodeAt();
+
+  //posiition =indice de la letra en el array
+  position=array.indexOf(word.charAt(i));
+
+  //aplicamos la formula dada
+  newPosition=(position+33)%26;
+
+  // obtenemos lanueva letra
+  newLetter+=array[newPosition];
+
 }
-//};
+return newLetter;
+
+}
+
+
+
+cipher("AB")
